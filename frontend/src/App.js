0e93498253821fcc1e8e709c18cffcc73ce00aab
NishanPokharel <nishanpokharel74@gmail.com>
2023-02-15 08:39:45 +0545

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 
+import Login from './pages/Login';
+import Home from './components/Home/Home.jsx'
+
 function App() {
   return (
     <div className="App">
+      {/* <Login /> */}
+      <Home />
     </div>
   );
 }
 
 export default App;
+  
