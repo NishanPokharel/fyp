abe9034951e41e0ef626a9d36fb669c20140013d
NishanPokharel <nishanpokharel74@gmail.com>
2023-02-15 08:39:38 +0545

    Create Footer.jsx

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Footer.css'
+
+function Footer() {
+  return (
+    <div className='footer'>
+        <a href='https://github.com/NishanPokharel/fyp'>
+          <img src='https://img.icons8.com/ios-glyphs/512/github.png' />
+        </a>
+
+        <a href='https://twitter.com/nishanpokh'>
+          <img src = 'https://www.freepnglogos.com/uploads/twitter-logo-png/twitter-logo-vector-png-clipart-1.png'/>
+        </a>
+    </div>
+  )
+}
+
+export default Footer
